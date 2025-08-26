47fd9147bd97de8ca8839b069f36dc2b9fb78882
KhushPatel018 <patelkhush.girishbhai@kickdrumtech.com>
2025-08-26 20:56:31 +0530

    first commit

---
+++ src/main/kotlin/com/kp18/GameBackend/GameBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.kp18.GameBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<GameBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/kp18/GameBackend/GameBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kp18.GameBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
